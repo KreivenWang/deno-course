a661fef3f74d553624ef8b0530225045738481fa
KreivenWang <kreiven.wang@hotmail.com>
2021-05-06 20:37:07 +0800

    init with index html and promises example calling jsonplaceholder

---
+++ promises.js
@@ -0,0 +1,14 @@
+async function sendRequest() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    console.log(response.status);
+    const exractedData = await response.json();
+    console.log(exractedData);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+sendRequest();
