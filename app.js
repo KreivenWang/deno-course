15d24266f4fbad04f8ed20e584d807047bfbde98
KreivenWang <kreiven.wang@hotmail.com>
2021-05-06 21:24:08 +0800

    implement myOwnAsyncIterator

--- app.js
+++ app.js
@@ -24,3 +24,39 @@ const myOwnIterable = {
 for (const iterator of myOwnIterable) {
   console.log(iterator);
 }
+
+const myOwnAsyncIterable = {
+  urls: [
+    "https://jsonplaceholder.typicode.com/todos/1",
+    "https://jsonplaceholder.typicode.com/todos/2",
+    "https://jsonplaceholder.typicode.com/todos/3",
+  ],
+  index: 0,
+  [Symbol.asyncIterator]() {
+    return this[Symbol.getAsyncIterator](this);
+  },
+  [Symbol.getAsyncIterator](self) {
+    return {
+      async next() {
+        if (self.index === self.urls.length) return { value: null, done: true };
+
+        const response = await fetch(self.urls[self.index]);
+        const exractedData = await response.json();
+        const result = {
+          value: exractedData,
+          done: false,
+        };
+        self.index++;
+        return result;
+      },
+    };
+  },
+};
+
+async function asyncLoop() {
+  for await (const iterator of myOwnAsyncIterable) {
+    console.log(iterator);
+  }
+}
+
+asyncLoop();
