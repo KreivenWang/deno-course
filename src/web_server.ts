ee8976590cf4a9fcbb649a587bb22a0f098f95f6
KreivenWang <kreiven.wang@hotmail.com>
2021-06-09 21:52:02 +0800

    clear import map, add color to log, set dist bundle folder

--- src/web_server.ts
+++ src/web_server.ts
@@ -1,6 +1,9 @@
 import { serve } from "https://deno.land/std@0.98.0/http/server.ts";
+import { bgBlue, red, bold } from "https://deno.land/std@0.98.0/fmt/colors.ts";
 const server = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of server) {
+  console.log(bgBlue("responding"));
   req.respond({ body: "Hello World\n things changed" });
+  console.log(red(bold("responded")));
 }
