1b34180df1199c9e048730288a282d2c7eaef382
KreivenWang <kreiven.wang@hotmail.com>
2021-06-12 13:09:49 +0800

    try debug; remove import map

--- src/web_server.ts
+++ src/web_server.ts
@@ -1,13 +1,24 @@
-// import { bgBlue, red, bold } from "https://deno.land/std@0.98.0/fmt/colors.ts";
-import { Application, Router } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+import {
+  Application,
+  Router,
+  send,
+} from "https://deno.land/x/oak@v7.5.0/mod.ts";
 import { renderFileToString } from "https://deno.land/x/dejs@0.9.3/mod.ts";
+
+interface Goal {
+  id: string;
+  name: string;
+}
 
 const app = new Application();
 const router = new Router();
 
+const courseGoals: Goal[] = [];
+
 router.get("/", async (ctx, next) => {
   const body = await renderFileToString(Deno.cwd() + "/src/course_goals.ejs", {
-    title: "My Goals",
+    title: "All Goals",
+    goals: courseGoals,
   });
   ctx.response.body = body;
   await next();
@@ -15,18 +26,24 @@ router.get("/", async (ctx, next) => {
 
 router.post("/add-goal", async (ctx, next) => {
   const body = ctx.request.body();
-  const newGoal = ((await body.value) as URLSearchParams)?.get("new-goal");
-  console.log(newGoal);
+  const params = (await body.value) as URLSearchParams;
+  const goalName = params?.get("new-goal") || "";
+  const newGoal: Goal = { id: new Date().toUTCString(), name: goalName };
+  courseGoals.push(newGoal);
   ctx.response.redirect("/");
   await next();
 });
 
 app.use(async (ctx, next) => {
-  console.log("start to use router");
+  if (ctx.request.url.pathname.endsWith(".css")) {
+    console.log("serve css", ctx.request.url.pathname);
+    await send(ctx, ctx.request.url.pathname, { gzip: true, root: "src" });
+  }
   await next();
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+console.log("App started at http://localhost:8000");
 await app.listen({ port: 8000 });
